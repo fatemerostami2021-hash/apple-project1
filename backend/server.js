6e6341d69bcacf66fb5a296b41449984bd40f1f5
fatemerostami2021-hash <rostamifatemeh.963@gmail.com>
2026-07-02 22:04:48 +0330

    fix: add x-migrate-token to CORS allowed headers

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "x-migrate-token"],
 }));
 
 app.use(helmet({ crossOriginEmbedderPolicy: false }));
